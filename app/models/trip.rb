77571c936a1c777781b77b01464e08fad3781241
Owen Xia <owenyx@gmail.com>
2015-05-18 14:37:07 +0800

    gem annotate

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: trips
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  begin_date :date
+#  end_date   :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  privacy    :string
+#
+
 class Trip < ActiveRecord::Base
 end
--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: trips
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  begin_date :date
+#  end_date   :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  privacy    :string
+#
+
 require 'test_helper'
 
 class TripTest < ActiveSupport::TestCase
